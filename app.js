c811d87fff365b73b71466711a991dd17607ef6d
anderson.morais <anderson.morais@hemoce.ce.gov.brgit config --global user.email anderson.morais@hemoce.ce.gov.br>
2024-12-09 16:11:14 -0300

    🗃️  config db

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { query } from 'express';
 import { createServer } from 'http';
 import db from './database.js';
 import path from 'path';
@@ -16,59 +16,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    let sessoes = [
-        {
-            id:54,
-            title:'sessão - 1',
-            perguntas:[
-                {
-                    id:25,
-                    title:'oq é a vida?'
-                },{
-                     id:26,
-                    title:'oq é um plateumito?'
-                },{
-                    id:27,
-                    title:'oq é um quinidario?' 
-                }
-            ]
-        },
-        {
-            id:55,
-            title:'sessão - 2',
-            perguntas:[
-                {
-                    id:78,
-                    title:'oq o sentido da vida'
-                },{
-                     id:79,
-                    title:'qual a resposta para tudo'
-                },{
-                    id:76,
-                    title:'quem é John Galte' 
-                }
-            ]
-        },
-        {
-            id:56,
-            title:'sessão - 3',
-            perguntas:[
-                {
-                    id:99,
-                    title:'Será que estamos sozinhos?'
-                },{
-                     id:26,
-                    title:'vc fez o L?'
-                },{
-                    id:27,
-                    title:'vc fez Arminha?' 
-                }
-            ]
-        },
-    ]
-    res.render('index',{sessoes})
+app.get('/', async (req, res) => {
+    const query = `
+        select * from sessao
+    `
+    const all = await db.run(query,[],err=>{
+        if(err){
+            console.log('Error na query')
+        }
+    })
+    console.log(all)
+    //res.render('index',{sessoes})
   });
+app.post('/admin/add/sessao',async (req,res)=>{
+    const query = `
+        insert into sessao ()
+    `
+    const sessao = await db.run(query,[],error=>{
+        if(err){
+            console.log('Error na query')
+        }
+    })
+})  
 
 // Inicia o servidor
 server.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
